62e88d52c3026aca5be45a753ebe0dba161bdda1
Prateek Jadhav <134202260+Prateek0712@users.noreply.github.com>
2024-04-11 09:13:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+
 function fibonacci(num) {
 // your code here
+	if(num-1<2)
+	{
+		return  num-1;
+	}
+	let feb1=0;
+	let feb2=1;
+	while(num-->2)
+		{
+			let temp=feb1+feb2;
+			feb1=feb2;
+			feb2=temp;
+		}
+	return feb2;
+	
 }
 
 module.exports = fibonacci;
